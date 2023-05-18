84486ecc98afe2a21d18caded2f3585e75f09d43
AndyKong <andy.kong@nuaa.edu.cn>
2023-05-18 16:44:44 +0800

    [feat]json2df2xlsx

--- main.py
+++ main.py
@@ -2,11 +2,18 @@ import asyncio
 import jsonLoader
 import pandas as pd
 
+
 def main():
     videos = jsonLoader.loadJsonFile("details.json")
     titles = jsonLoader.getJsonData(videos, "title")
-    for video in videos:
-        print(video["title"])
+    bvids = jsonLoader.getJsonData(videos, "bvid")
+    urls = ['https://www.bilibili.com/video/' + bvid for bvid in bvids]
+    df = pd.DataFrame({"title": titles, "bvid": bvids, "url": urls})
+    print(df)
+    df.to_excel("details.xlsx", index=False)
+    # print(titles)
+    # for video in videos:
+    #     print(video["title"])
 
 
 if __name__ == '__main__':
