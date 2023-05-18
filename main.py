b3f7fdb1ca359d5a5ba33be9607a9e46ce126c52
AndyKong <andy.kong@nuaa.edu.cn>
2023-05-18 13:40:00 +0800

    1

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import asyncio
-from bilibili_api import video
+import jsonLoader
+import pandas as pd
 
-async def main() -> None:
-    # 实例化 Video 类
-    v = video.Video(bvid="BV1uv411q7Mv")
-    # 获取信息
-    info = await v.get_info()
-    # 打印信息
-    print(info)
+def main():
+    videos = jsonLoader.loadJsonFile("details.json")
+    titles = jsonLoader.getJsonData(videos, "title")
+    for video in videos:
+        print(video["title"])
+
 
 if __name__ == '__main__':
-    asyncio.get_event_loop().run_until_complete(main())
+    main()
